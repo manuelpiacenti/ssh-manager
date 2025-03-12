8849c9818c73eeebb0f6e957f6cd35037e13f58d
Manuel Piacenti <manuel.piacenti@gmail.com>
2025-03-12 12:46:50 +0100

    Move README.md to root and update changelog for v0.5.0

---
+++ ssh_manager/setup.py
@@ -0,0 +1,28 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='ssh-manager',
+    version='0.5.0',
+    packages=find_packages(),
+    install_requires=[
+        'python-dotenv',
+        'questionary',
+        'gitpython'
+    ],
+    entry_points={
+        'console_scripts': [
+            'ssh-manager=ssh_manager.cli:main'
+        ],
+    },
+    include_package_data=True,
+    description='Manage SSH configs by group and subgroup from the command line',
+    author='Manuel Piacenti',
+    license='GPLv3',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
