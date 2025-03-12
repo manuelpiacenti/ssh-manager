dd622a873fc23a6eda0b351da310e15500481f9a
Manuel Piacenti <manuel.piacenti@gmail.com>
2025-03-12 12:48:06 +0100

    Release v0.5.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ssh-manager',
-    version='0.5.0',
+    version='0.5.1',
     packages=find_packages(),
     install_requires=[
         'python-dotenv',
